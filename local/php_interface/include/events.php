3233dda43f23c22d95ac8787bde48221dc30929d
Nemor89 <express-don@mail.ru>
2021-04-04 18:24:31 +0300

    [ex2-93]

--- local/php_interface/include/events.php
+++ local/php_interface/include/events.php
@@ -1,5 +1,6 @@
 <?php
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("EX2", "EX2_50"));
+AddEventHandler("main", "OnEpilog", Array("EX2", "EX2_93"));
 IncludeModuleLangFile(__FILE__);
 
 class EX2
@@ -30,6 +31,25 @@ class EX2
                     return false;
                 }
             }
+        }
+    }
+    function EX2_93()
+    {
+        if (defined("ERROR_404") && ERROR_404 == "Y")
+        {
+            global $APPLICATION;
+            $APPLICATION->RestartBuffer();
+            include $_SERVER["DOCUMENT_ROOT"] . SITE_TEMPLATE_PATH . "/header.php";
+            include $_SERVER["DOCUMENT_ROOT"] . "/404.php";
+            include $_SERVER["DOCUMENT_ROOT"] . SITE_TEMPLATE_PATH . "/footer.php";
+            CEventLog::Add(
+                [
+                    "SEVERITY" => "INFO",
+                    "AUDIT_TYPE_ID" => "ERROR_404",
+                    "MODULE_ID" => "main",
+                    "DESCRIPTION" => $APPLICATION->GetCurPage()
+                ]
+            );
         }
     }
 }
