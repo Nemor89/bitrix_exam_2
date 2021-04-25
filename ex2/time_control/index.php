14b0ff17629841cfb695e702d73ed7d35c2d421a
Nemor89 <express-don@mail.ru>
2021-04-25 16:37:34 +0300

    [ex2-88]

---
+++ ex2/time_control/index.php
@@ -0,0 +1,12 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>
+Самая ресурсоемкая страница - /products/index.php, доля нагрузки - 16.96%
+<br>
+Работа компонента по умолчанию - 7кб
+<br>
+Работа компонента. Только необходимые параметры - 4кб
+<br>
+Разница: 7кб - 4кб = 3кб
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
