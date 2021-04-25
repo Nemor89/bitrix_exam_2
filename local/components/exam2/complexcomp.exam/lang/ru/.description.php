ab19065a05c5ac225d5a70a9a02de13a9d7d9ef5
Nemor89 <express-don@mail.ru>
2021-04-25 17:37:14 +0300

    [ex2-25]

---
+++ local/components/exam2/complexcomp.exam/lang/ru/.description.php
@@ -0,0 +1,7 @@
+<?
+$MESS ['IBLOCK_PHOTO_NAME'] = "Мой комплексный компонент (материалы к экзамену)";
+$MESS ['IBLOCK_PHOTO_SECTION'] = "Экзамен №2";
+$MESS ['IBLOCK_PHOTO_DESCRIPTION'] = "Полная фотогалерея";
+$MESS ['T_IBLOCK_DESC_PHOTO'] = "Фотогалерея";
+$MESS["SIMPLECOMP_EXAM2_PATH"] = "Второй экзамен";
+?>
